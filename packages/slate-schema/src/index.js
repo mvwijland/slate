f7a134f80e72bb59abe39b4149b0cddf51a335d8
Soreine <soreine.plume@gmail.com>
2018-09-20 14:37:50 +0200

    Init folder structure

---
+++ packages/slate-schema/src/index.js
@@ -0,0 +1,5 @@
+/**
+ * Export.
+ */
+
+export default {}
---
+++ packages/slate-schema/test/index.js
@@ -0,0 +1,16 @@
+/**
+ * Dependencies.
+ */
+
+import assert from 'assert'
+import SlateSchema from '..'
+
+/**
+ * Tests.
+ */
+
+describe('slate-hyperscript', () => {
+  it('should import correctly', () => {
+    assert.ok(SlateSchema)
+  })
+})
