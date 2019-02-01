50d0f9b8a5ebf42102b08469e77ced16925e6f41
Soreine <soreine.plume@gmail.com>
2019-02-01 16:53:16 +0100

    Add new packages to rollup config

--- support/rollup/config.js
+++ support/rollup/config.js
@@ -11,6 +11,13 @@ import slatePropTypes from '../../packages/slate-prop-types/package.json'
 import slateReact from '../../packages/slate-react/package.json'
 import slateSchemaViolations from '../../packages/slate-schema-violations/package.json'
 import slateSimulator from '../../packages/slate-simulator/package.json'
+import slateEditTable from '../../packages/slate-edit-table/package.json'
+import slateEditCode from '../../packages/slate-edit-code/package.json'
+import slateEditList from '../../packages/slate-edit-list/package.json'
+import slateEditBlockquote from '../../packages/slate-edit-blockquote/package.json'
+import slatePrism from '../../packages/slate-prism/package.json'
+import slateAutoReplace from '../../packages/slate-auto-replace/package.json'
+
 // Do not import slateDevBenchmark here. The benchmark shall be a pure nodeJS program and can be run without babel-node
 
 const configurations = [
@@ -26,6 +33,12 @@ const configurations = [
   ...factory(slateReact),
   ...factory(slateSchemaViolations),
   ...factory(slateSimulator),
+  ...factory(slateEditTable),
+  ...factory(slateEditCode),
+  ...factory(slateEditList),
+  ...factory(slateEditBlockquote),
+  ...factory(slatePrism),
+  ...factory(slateAutoReplace),
 ]
 
 export default configurations
